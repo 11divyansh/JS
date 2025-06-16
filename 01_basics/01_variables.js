dcdf8cbc2f12535004af10a4af674cc58610db52
11divyansh <122454541+11divyansh@users.noreply.github.com>
2025-06-16 17:13:41 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "bhatt11divyansh@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 //not allowed
+accountEmail="b11divyansh@gmail.com"
+accountPass="2132"
+accountCity="delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity,accountState])
+/*
+Prefer not to use 'var' because of issue in
+block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) // we are using nodejs, not browser
+
+let name="divyansh"
+let age=18
+let isLoggedIn=false
+let state;
+
+// number=> 2^53
+// bigint
+// string=>""
+// boolean=> true/false
+// null is a standalone value
+// undefined=>
+// symbol=>unique
+
+// object
+
+console.log(typeof "hello");
+console.log(typeof null);
+console.log(typeof undefined);
